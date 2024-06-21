6084ce2da856a256a7b57988eb9982694b6a20d7
angelrosales1811 <angeldesarrolloweb@gmail.com>
2024-06-20 20:18:16 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyC4D4r-LD7tSp1En9vMXuXerAGIp1HuwrM',
+    authDomain: 'login-arj.firebaseapp.com',
+    projectId: 'login-arj',
+    storageBucket: 'login-arj.appspot.com',
+    messagingSenderId: '39531972908',
+    appId: '1:39531972908:web:80f435c4712729c73e0489'
+  },
+};
